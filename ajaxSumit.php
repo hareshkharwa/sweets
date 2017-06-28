35b2519044d149f289958c454f35dbdd3195560f
hareshkharwa <hareshkharwa@gmail.com>
2017-06-28 19:07:28 +0530

    Update ajaxSumit.php

--- ajaxSumit.php
+++ ajaxSumit.php
@@ -1,28 +1,17 @@
 <?php
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['messageField'];
+if($_POST["submit"]) {
+    $recipient="hareshkharwa@gmail.com";
+    $subject="Form to email message";
+    $sender=$_POST["name"];
+    $senderEmail=$_POST["email"];
+    $message=$_POST["message"];
 
-$header = 'From: ' . $email . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .=  "Mime-Version: 1.0  \r\n";
-$header .= "Content-Type: text/plain";
+    $mailBody="Name: $name\nEmail: $email\n\n$message";
 
-$comment .= "This message was sent by: " . $name . " \r\n";
-$comment .= "His email is: " . $email . " \r\n";
-$comment .= "The message is: ". $message ." \r\n";
+    mail($recipient, $subject, $mailBody, "From: $name <$email>");
 
-$for = 'hareshkharwa@gmail.com';
-$subject = 'You have a new email from your website!';
-
-mail($for, $subject, utf8_decode($comment), $header);
-
-// server response!
-$nombre = $_POST['nombre'];
-
-echo json_encode(array(
-	'message' => sprintf('Your email has been received %s', $name),
-));
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
 
 ?>
